ab195110f0b01437a09ba880fdb163f20f294ba2
Rezzy Yuhand <rezzyyuhand92@student.ub.ac.id>
2023-11-25 22:19:48 +0700

    initial commit

---
+++ app/src/main/java/com/example/pulsar/MainActivity.kt
@@ -0,0 +1,98 @@
+package com.example.pulsar
+
+import android.Manifest
+import android.app.Activity
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.content.Intent
+import android.content.SharedPreferences
+import android.os.Build
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.pulsar.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val TAG = "Polar_MainActivity"
+        private const val SHARED_PREFS_KEY = "polar_device_id"
+        private const val PERMISSION_REQUEST_CODE = 1
+    }
+
+    private lateinit var sharedPreferences: SharedPreferences
+    private val bluetoothOnActivityResultLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+        if (result.resultCode != Activity.RESULT_OK) {
+            Log.w(TAG, "Bluetooth off")
+        }
+    }
+
+    private lateinit var binding: ActivityMainBinding
+
+    private var deviceId: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Thread.sleep(2000)
+
+        supportActionBar?.hide()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        sharedPreferences = getPreferences(MODE_PRIVATE)
+        deviceId = sharedPreferences.getString(SHARED_PREFS_KEY, "")
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+
+    private fun checkBT() {
+        val btManager = applicationContext.getSystemService(BLUETOOTH_SERVICE) as BluetoothManager
+        val bluetoothAdapter: BluetoothAdapter? = btManager.adapter
+        if (bluetoothAdapter == null) {
+            showToast("Device doesn't support Bluetooth")
+            return
+        }
+
+        if (!bluetoothAdapter.isEnabled) {
+            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            bluetoothOnActivityResultLauncher.launch(enableBtIntent)
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                requestPermissions(arrayOf(Manifest.permission.BLUETOOTH_SCAN, Manifest.permission.BLUETOOTH_CONNECT), PERMISSION_REQUEST_CODE)
+            } else {
+                requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), PERMISSION_REQUEST_CODE)
+            }
+        } else {
+            requestPermissions(arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION), PERMISSION_REQUEST_CODE)
+        }
+    }
+
+    private fun showToast(message: String) {
+        val toast = Toast.makeText(applicationContext, message, Toast.LENGTH_LONG)
+        toast.show()
+    }
+}
