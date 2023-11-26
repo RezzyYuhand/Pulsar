79f7d6841ab8a0993f8c37fa837eb4cdbc76caf7
Rezzy Yuhand <rezzyyuhand92@student.ub.ac.id>
2023-11-26 17:34:55 +0700

    update night template

--- app/src/main/java/com/example/pulsar/ui/dashboard/DashboardViewModel.kt
+++ app/src/main/java/com/example/pulsar/ui/dashboard/DashboardViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class DashboardViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "Please Insert you device ID to continue"
     }
     val text: LiveData<String> = _text
 }
